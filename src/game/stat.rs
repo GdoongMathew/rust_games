60162915408e70cba06ffdaf3c125fd95e3058e4
GdoongMathew <gdoongmathew@gmail.com>
2024-07-20 16:47:54 +0800

    test: add `test_stat_display`, `test_stat_sub`, and `test_stat_add_assign`.

--- src/game/stat.rs
+++ src/game/stat.rs
@@ -82,6 +82,18 @@ mod tests {
     use parameterized::ide;
     use parameterized::parameterized as pm;
 
+    #[test]
+    fn test_stat_display(){
+        let stat = Stat {
+            health: 100,
+            attack: 100,
+            magic: 100,
+            defense: 100
+        };
+
+        assert_eq!(format!("{}", stat), "State: [health: 100, attack: 100, defense: 100, magic: 100]\n");
+    }
+
     mod stat_tests {
         use super::*;
         ide!();
@@ -105,6 +117,25 @@ mod tests {
             assert_eq!(stat + stat2, expect)
         }
 
+        #[pm(stat = {
+            Stat {health: 10, attack: 10, defense: 10, magic: 10},
+            Stat {health: 0, attack: 0, defense: 0, magic: 0},
+            Stat {health: -10, attack: -10, defense: -10, magic: -10},
+        }, expect = {
+            Stat {health: 20, attack: 10, defense: -90, magic: -9},
+            Stat {health: 10, attack: 0, defense: -100, magic: -19},
+            Stat {health: 0, attack: -10, defense: -110, magic: -29},
+        })]
+        fn test_stat_sub(stat: Stat, expect: Stat) {
+            let stat2 = Stat {
+                health: -10,
+                attack: 0,
+                defense: 100,
+                magic: 19,
+            };
+            assert_eq!(stat - stat2, expect)
+        }
+
         #[test]
         fn test_stat_add_assign() {
             let mut stat = Stat {
@@ -122,6 +153,24 @@ mod tests {
             };
 
             stat += stat2;
+            assert_eq!(stat, Stat{health: 0, attack: 0, defense: 0, magic: 0})
+        }
+
+        #[test]
+        fn test_stat_sub_assign(){
+            let mut stat = Stat {
+                health: 10,
+                attack: 10,
+                defense: 10,
+                magic: 10,
+            };
+            let stat2 = Stat {
+                health: 10,
+                attack: 10,
+                defense: 10,
+                magic: 10,
+            };
+            stat -= stat2;
             assert_eq!(stat, Stat{health: 0, attack: 0, defense: 0, magic: 0})
         }
     }
