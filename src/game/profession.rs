c02bb7e4b257380d767a0638f63437281ed017b3
GdoongMathew <gdoongmathew@gmail.com>
2024-07-27 18:30:08 +0800

    refactor: add `attack_points` and `defense_points` in Profession so that profession can decide which stat points should be the attackpoints.

--- src/game/profession.rs
+++ src/game/profession.rs
@@ -1,9 +1,12 @@
-use crate::game::stat::Stat;
+use crate::game::stat::{Stat, StatTrait};
 
 pub trait Profession {
     fn profession_type(&self) -> ProfessionType;
     fn effective_against<P: Profession>(&self, profession: &P) -> bool;
     fn suppressed_by<P: Profession>(&self, profession: &P) -> bool;
+
+    fn attack_points<T: StatTrait>(&self, stat_trait: &T) -> i32;
+    fn defense_points<T: StatTrait>(&self, stat_trait: &T) -> i32;
 }
 
 pub struct Warrior {
@@ -26,14 +29,22 @@ impl Warrior {
 
 impl Profession for Warrior {
     fn profession_type(&self) -> ProfessionType {
-        ProfessionType::Warrior
+        ProfessionType::WarriorType
     }
     fn effective_against<P: Profession>(&self, profession: &P) -> bool {
-        matches! (profession.profession_type(), ProfessionType::Knight)
+        matches! (profession.profession_type(), ProfessionType::KnightType)
     }
 
     fn suppressed_by<P: Profession>(&self, profession: &P) -> bool {
-        matches! (profession.profession_type(), ProfessionType::Sorcerer)
+        matches! (profession.profession_type(), ProfessionType::SorcererType)
+    }
+
+    fn attack_points<T: StatTrait>(&self, stat_trait: &T) -> i32 {
+        stat_trait.get_stat().attack
+    }
+
+    fn defense_points<T: StatTrait>(&self, stat_trait: &T) -> i32 {
+        stat_trait.get_stat().defense
     }
 }
 
@@ -56,17 +67,24 @@ impl Sorcerer {
 
 impl Profession for Sorcerer {
     fn profession_type(&self) -> ProfessionType {
-        ProfessionType::Sorcerer
+        ProfessionType::SorcererType
     }
 
     fn effective_against<P: Profession>(&self, profession: &P) -> bool {
-        matches! (profession.profession_type(), ProfessionType::Warrior)
+        matches! (profession.profession_type(), ProfessionType::WarriorType)
     }
 
     fn suppressed_by<P: Profession>(&self, profession: &P) -> bool {
-        matches! (profession.profession_type(), ProfessionType::Knight)
+        matches! (profession.profession_type(), ProfessionType::KnightType)
+    }
+    fn attack_points<T: StatTrait>(&self, stat_trait: &T) -> i32 {
+        stat_trait.get_stat().magic
+    }
+    fn defense_points<T: StatTrait>(&self, stat_trait: &T) -> i32 {
+        stat_trait.get_stat().defense
     }
 }
+
 
 pub struct Knight {
     stat: Stat,
@@ -87,23 +105,29 @@ impl Knight {
 
 impl Profession for Knight {
     fn profession_type(&self) -> ProfessionType {
-        ProfessionType::Knight
+        ProfessionType::KnightType
     }
     fn effective_against<P: Profession>(&self, profession: &P) -> bool {
-        matches! (profession.profession_type(), ProfessionType::Sorcerer)
+        matches! (profession.profession_type(), ProfessionType::SorcererType)
     }
 
     fn suppressed_by<P: Profession>(&self, profession: &P) -> bool {
-        matches! (profession.profession_type(), ProfessionType::Warrior)
+        matches! (profession.profession_type(), ProfessionType::WarriorType)
+    }
+    fn attack_points<T: StatTrait>(&self, stat_trait: &T) -> i32 {
+        stat_trait.get_stat().attack
+    }
+    fn defense_points<T: StatTrait>(&self, stat_trait: &T) -> i32 {
+        stat_trait.get_stat().defense
     }
 }
 
 
 #[derive(Debug, PartialEq)]
 enum ProfessionType {
-    Warrior,
-    Sorcerer,
-    Knight,
+    WarriorType,
+    SorcererType,
+    KnightType,
 }
 
 
@@ -132,19 +156,19 @@ mod profession_tests {
     #[test]
     fn test_warrior_profession_type() {
         let warrior = Warrior::new();
-        assert_eq!(warrior.profession_type(), ProfessionType::Warrior);
+        assert_eq!(warrior.profession_type(), ProfessionType::WarriorType);
     }
 
     #[test]
     fn test_sorcerer_profession_type() {
         let sorcerer = Sorcerer::new();
-        assert_eq!(sorcerer.profession_type(), ProfessionType::Sorcerer);
+        assert_eq!(sorcerer.profession_type(), ProfessionType::SorcererType);
     }
 
     #[test]
     fn test_knight_profession_type() {
         let knight = Knight::new();
-        assert_eq!(knight.profession_type(), ProfessionType::Knight);
+        assert_eq!(knight.profession_type(), ProfessionType::KnightType);
     }
 
     #[test]
@@ -199,5 +223,59 @@ mod profession_tests {
         let sorcerer = Sorcerer::new();
         assert_eq!(knight.suppressed_by(&sorcerer), false);
         assert_eq!(knight.suppressed_by(&warrior), true);
+    }
+
+    struct TestProp;
+
+    impl StatTrait for TestProp {
+        fn get_stat(&self) -> Stat {
+            Stat{health: 10, attack: 11, defense: 12, magic: 13}
+        }
+
+        fn set_stat(&self, stat: Stat) -> () {
+            todo!()
+        }
+    }
+
+    #[test]
+    fn test_sorcerer_attack_points() {
+        let prop = TestProp{};
+        let prof = Sorcerer::new();
+        assert_eq!(prof.attack_points(&prop), prop.get_stat().magic)
+    }
+
+    #[test]
+    fn test_sorcerer_defense_points() {
+        let prop = TestProp{};
+        let prof = Sorcerer::new();
+        assert_eq!(prof.defense_points(&prop), prop.get_stat().defense)
+    }
+
+    #[test]
+    fn test_knight_attack_points() {
+        let prop = TestProp{};
+        let prof = Knight::new();
+        assert_eq!(prof.attack_points(&prop), prop.get_stat().attack)
+    }
+
+    #[test]
+    fn test_knight_defense_points() {
+        let prop = TestProp{};
+        let prof = Knight::new();
+        assert_eq!(prof.defense_points(&prop), prop.get_stat().defense)
+    }
+
+    #[test]
+    fn test_warrior_attack_points() {
+        let prop = TestProp{};
+        let prof = Warrior::new();
+        assert_eq!(prof.attack_points(&prop), prop.get_stat().attack)
+    }
+
+    #[test]
+    fn test_warrior_defense_points() {
+        let prop = TestProp{};
+        let prof = Warrior::new();
+        assert_eq!(prof.defense_points(&prop), prop.get_stat().defense)
     }
 }
